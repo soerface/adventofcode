a74695bd2f5ecc7807e7af70745a1001de0c27e7
Soeren Wegener <wegener92@gmail.com>
2020-12-17 07:26:13 +0100

    Day 17.2 (WIP)

--- 17.py
+++ 17.py
@@ -8,11 +8,9 @@ import numpy as np
 def apply_rules(array: np.ndarray) -> np.ndarray:
     new = np.zeros(shape=array.shape, dtype=int)
     w, h, d = array.shape
-    n = 0
     for x in range(w):
         for y in range(h):
             for z in range(d):
-                n += 1
                 slced = array[x - 1:x + 2, y - 1:y + 2, z - 1:z + 2]
                 slc = slced.flatten()
                 if array[x, y, z]:
@@ -22,23 +20,49 @@ def apply_rules(array: np.ndarray) -> np.ndarray:
     return new
 
 
+def apply_rules2(array: np.ndarray) -> np.ndarray:
+    """
+    Doesn't work
+    """
+    new = np.zeros(shape=array.shape, dtype=int)
+    w, h, d, d2 = array.shape
+    for x in range(w):
+        for y in range(h):
+            for z in range(d):
+                for u in range(d2):
+                    slced = array[x - 1:x + 2, y - 1:y + 2, z - 1:z + 2, u - 1:u + 2]
+                    slc = slced.flatten()
+                    if array[x, y, z, u]:
+                        new[x, y, z, u] = 2 <= len(slc[slc == 1]) - 1 <= 3
+                    else:
+                        new[x, y, z, u] = len(slc[slc == 1]) == 3
+    return new
+
+
 def main_1(inp: List[str]):
     array = np.zeros(shape=(len(inp), len(inp), len(inp)), dtype=int)
     array[0] = [[x == "#" for x in l.strip()] for l in inp]
-    # array = np.array(lst, dtype=int)
-    array = np.pad(array, 15)
+    array = np.pad(array, 1)
     for _ in range(6):
+        array = np.pad(array, 1)
         array = apply_rules(array)
     ar = array.flatten()
     return len(ar[ar == 1])
 
 
 def main_2(inp: List[str]):
-    return
+    array = np.zeros(shape=(len(inp), len(inp), len(inp), len(inp)), dtype=int)
+    array[0] = [[x == "#" for x in l.strip()] for l in inp]
+    array = np.pad(array, 1)
+    for _ in range(6):
+        array = np.pad(array, 1)
+        array = apply_rules2(array)
+    ar = array.flatten()
+    return len(ar[ar == 1])
 
 
 if __name__ == "__main__":
-    with open("17.txt") as f:
+    with open("17.example.txt") as f:
         inp = f.readlines()
     print(main_1(inp))
     print(main_2(inp))
